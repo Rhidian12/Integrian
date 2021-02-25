6200dc88746e2ff793c557e4793b7f24ece8b808
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-02-25 13:14:27 +0100

    Added InputComponent

---
+++ Integrian/InputComponent.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef INTEGRIAN_INPUTCOMPONENT_H
+#define INTEGRIAN_INPUTCOMPONENT_H
+
+#include "Component.h"
+
+namespace Integrian
+{
+	class InputComponent : public Component
+	{
+	};
+}
+
+#endif // !INTEGRIAN_INPUTCOMPONENT_H
