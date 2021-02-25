a70a838e65fc48043b78b6ea278bbf9a0ad1dea3
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-02-25 14:00:17 +0100

    Implemented Inputcomponent

--- Integrian/InputComponent.cpp
+++ Integrian/InputComponent.cpp
@@ -1,1 +1,25 @@
+#include "pch.h"
 #include "InputComponent.h"
+#include "InputManager.h"
+
+void Integrian::InputComponent::AddInput(const GameInput& gameInput, Command* pCommand, const State keyState, const uint8_t controllerIndex)
+{
+	InputManager::GetInstance().AddCommand(gameInput, pCommand, keyState, controllerIndex);
+}
+
+bool Integrian::InputComponent::IsPressed(const GameInput& gameInput, const uint8_t playerIndex) const
+{
+	if (gameInput.controllerInput != ControllerInput::INVALID)
+		return InputManager::GetInstance().IsControllerButtonPressed(gameInput.controllerInput, playerIndex);
+
+	if (gameInput.keyboardInput != KeyboardInput::INVALID)
+		return InputManager::GetInstance().IsKeyboardKeyPressed(gameInput.keyboardInput);
+
+	if (gameInput.mouseButton != MouseButton::INVALID)
+		return InputManager::GetInstance().IsMouseButtonPressed(gameInput.mouseButton);
+}
+
+const Integrian::Point2f Integrian::InputComponent::GetMousePosition() const
+{
+	return InputManager::GetInstance().GetMousePosition();
+}
--- Integrian/InputComponent.h
+++ Integrian/InputComponent.h
@@ -3,11 +3,23 @@
 #define INTEGRIAN_INPUTCOMPONENT_H
 
 #include "Component.h"
+#include "GameInput.h"
 
 namespace Integrian
 {
+	class Command;
 	class InputComponent : public Component
 	{
+	public:
+		InputComponent() = default;
+		virtual ~InputComponent() = default;
+
+		void AddInput(const GameInput& gameInput, Command* pCommand, const State keyState, const uint8_t controllerIndex = 0);
+		[[nodiscard]] bool IsPressed(const GameInput& gameInput, const uint8_t playerIndex = 0) const;
+		[[nodiscard]] const Point2f GetMousePosition() const;
+
+	private:
+		
 	};
 }
 
