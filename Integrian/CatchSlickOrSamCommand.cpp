bb78b818e9168c2443987f9bc7938d5d45c671c9
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-02-26 14:52:19 +0100

    Added CatchSlickOrSamCommand

---
+++ Integrian/CatchSlickOrSamCommand.cpp
@@ -0,0 +1,1 @@
+#include "CatchSlickOrSamCommand.h"
---
+++ Integrian/CatchSlickOrSamCommand.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef INTEGRIAN_CATCHSLICKORSAMCOMMAND_H
+#define INTEGRIAN_CATCHSLICKORSAMCOMMAND_H
+
+namespace Integrian
+{
+	class CatchSlickOrSamCommand
+	{
+	};
+}
+
+#endif // !INTEGRIAN_CATCHSLICKORSAMCOMMAND_H
