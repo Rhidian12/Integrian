a0bb50fbffec7004bf11ab10592cdb5bd903c8ce
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-02-26 15:08:58 +0100

    Implemented ChangeColourCommand

--- Integrian/ChangeColourCommand.cpp
+++ Integrian/ChangeColourCommand.cpp
@@ -1,1 +1,12 @@
 #include "ChangeColourCommand.h"
+#include "ScoreComponent.h"
+
+Integrian::ChangeColourCommand::ChangeColourCommand(ScoreComponent* pScoreComponent)
+	: m_pScoreComponent{ pScoreComponent }
+{
+}
+
+void Integrian::ChangeColourCommand::Execute()
+{
+	m_pScoreComponent->ChangeScore(TypeOfGettingScore::ColourChange);
+}
--- Integrian/ChangeColourCommand.h
+++ Integrian/ChangeColourCommand.h
@@ -2,8 +2,22 @@
 #ifndef INTEGRIAN_CHANGECOLOURCOMMAND_H
 #define INTEGRIAN_CHANGECOLOURCOMMAND_H
 
-class ChangeColourCommand
+#include "Command.h"
+
+namespace Integrian
 {
-};
+	class ScoreComponent;
+	class ChangeColourCommand final : public Command
+	{
+	public:
+		ChangeColourCommand(ScoreComponent* pScoreComponent);
+		virtual ~ChangeColourCommand() = default;
+
+		virtual void Execute() override;
+	
+	private:
+		ScoreComponent* m_pScoreComponent;
+	};
+}
 
 #endif // !INTEGRIAN_CHANGECOLOURCOMMAND_H
