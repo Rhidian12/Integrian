3994aa6e121007624d237d97206b37e32351d55b
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-02-26 14:21:23 +0100

    Added ScoreDisplayComponent

---
+++ Integrian/ScoreDisplayComponent.cpp
@@ -0,0 +1,1 @@
+#include "ScoreDisplayComponent.h"
---
+++ Integrian/ScoreDisplayComponent.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef INTEGRIAN_SCOREDISPLAYCOMPONENT_H
+#define INTEGRIAN_SCOREDISPLAYCOMPONENT_H
+
+#include "Component.h"
+
+namespace Integrian
+{
+	class ScoreDisplayComponent final : public Component
+	{
+	public:
+
+	private:
+
+	};
+}
+
+#endif // !INTEGRIAN_SCOREDISPLAYCOMPONENT_H
