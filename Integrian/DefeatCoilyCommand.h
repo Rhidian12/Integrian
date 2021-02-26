81e9edea664d3073207a8ea76384132d90109db9
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-02-26 14:48:28 +0100

    Added DefeatCoilyCommand

---
+++ Integrian/DefeatCoilyCommand.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef INTEGRIAN_DEFEATCOILYCOMMAND_H
+#define INTEGRIAN_DEFEATCOILYCOMMAND_H
+
+#include "Command.h"
+
+namespace Integrian
+{
+	class DefeatCoilyCommand final : public Command
+	{
+	}
+};
+
+#endif // !INTEGRIAN_DEFEATCOILYCOMMAND_H
