eae2afb492de97a481ee0d894fa250d249f67a7a
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-02-26 14:26:48 +0100

    Added Score Component

---
+++ Integrian/ScoreComponent.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef INTEGRIAN_SCORECOMPONENT_H
+#define INTEGRIAN_SCORECOMPONENT_H
+
+#include "Component.h"
+
+namespace Integrian
+{
+	class ScoreComponent final : public Component
+	{
+	};
+}
+
+#endif // !INTEGRIAN_SCORECOMPONENT_H
