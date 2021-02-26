942d58fb164799fc432b221c95e731330b96e0a9
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-02-26 14:54:17 +0100

    Implemented CatchSlickOrSlamCommand

--- Integrian/CatchSlickOrSamCommand.cpp
+++ Integrian/CatchSlickOrSamCommand.cpp
@@ -1,1 +1,12 @@
 #include "CatchSlickOrSamCommand.h"
+#include "ScoreComponent.h"
+
+Integrian::CatchSlickOrSamCommand::CatchSlickOrSamCommand(ScoreComponent* pScoreComponent)
+	: m_pScoreComponent{ pScoreComponent }
+{
+}
+
+void Integrian::CatchSlickOrSamCommand::Execute()
+{
+	m_pScoreComponent->ChangeScore(TypeOfGettingScore::CatchingSlickAndSam);
+}
--- Integrian/CatchSlickOrSamCommand.h
+++ Integrian/CatchSlickOrSamCommand.h
@@ -2,10 +2,21 @@
 #ifndef INTEGRIAN_CATCHSLICKORSAMCOMMAND_H
 #define INTEGRIAN_CATCHSLICKORSAMCOMMAND_H
 
+#include "Command.h"
+
 namespace Integrian
 {
-	class CatchSlickOrSamCommand
+	class ScoreComponent;
+	class CatchSlickOrSamCommand final : public Command
 	{
+	public:
+		CatchSlickOrSamCommand(ScoreComponent* pScoreComponent);
+		virtual ~CatchSlickOrSamCommand() = default;
+
+		virtual void Execute() override;
+
+	private:
+		ScoreComponent* m_pScoreComponent;
 	};
 }
 
