83751ef51302868590e6463273f96548537ca1ac
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-02-26 15:09:58 +0100

    Added DisksRemainingCommand

---
+++ Integrian/DisksRemainingCommand.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef INTEGRIAN_DISKSREMAININGCOMMAND_H
+#define INTEGRIAN_DISKSREMAININGCOMMAND_H
+
+#include "Command.h"
+
+namespace Integrian
+{
+	class DisksRemainingCommand
+	{
+	};
+}
+
+#endif // !INTEGRIAN_DISKSREMAININGCOMMAND_H
