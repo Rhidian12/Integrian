194ab73f19c67645f2b64a497790736f2923fad2
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-03-17 15:02:39 +0100

    Made proper Getters for ServiceLocator

--- Integrian/ServiceLocator.cpp
+++ Integrian/ServiceLocator.cpp
@@ -1,1 +1,26 @@
 #include "ServiceLocator.h"
+
+const Integrian::InputManager& Integrian::ServiceLocator::GetInputManager() const
+{
+    return m_Inputmanager;
+}
+
+const Integrian::CommandManager& Integrian::ServiceLocator::GetCommandManager() const
+{
+    return m_CommandManager;
+}
+
+const Integrian::PrinterManager& Integrian::ServiceLocator::GetPrinterManager() const
+{
+    return m_PrinterManager;
+}
+
+const Integrian::TextureManager& Integrian::ServiceLocator::GetTextureManager() const
+{
+    return m_TextureManager;
+}
+
+const Integrian::Logger& Integrian::ServiceLocator::GetLogger() const
+{
+    return m_Logger;
+}
--- Integrian/ServiceLocator.h
+++ Integrian/ServiceLocator.h
@@ -13,13 +13,20 @@ namespace Integrian
 	class ServiceLocator final : public Singleton<ServiceLocator>
 	{
 	public:
+		const InputManager& GetInputManager() const;
+		const CommandManager& GetCommandManager() const;
+		const PrinterManager& GetPrinterManager() const;
+		const TextureManager& GetTextureManager() const;
+		const Logger& GetLogger() const;
+
+	private:
+		friend class Singleton<ServiceLocator>;
+
 		InputManager m_Inputmanager{ __FILE__, __LINE__ };
 		CommandManager m_CommandManager{ __FILE__, __LINE__ };
 		PrinterManager m_PrinterManager{ __FILE__, __LINE__ };
 		TextureManager m_TextureManager{ __FILE__, __LINE__ };
-
-	private:
-		friend class Singleton<ServiceLocator>;
+		Logger m_Logger{ __FILE__, __LINE__ };
 	};
 }
 
