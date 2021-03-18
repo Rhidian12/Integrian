2065a85f4918a9ad9b997c093dad215aff301420
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-03-18 11:16:32 +0100

    Added service locator

---
+++ Integrian/ServiceLocator.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifndef INTEGRIAN_SERVICELOCATOR_H
+#define INTEGRIAN_SERVICELOCATOR_H
+
+namespace Integrian
+{
+	class ServiceLocator
+	{
+	};
+}
+
+#endif // !INTEGRIAN_SERVICELOCATOR_H
