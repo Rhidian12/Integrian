872810fe7b3e1d28c987074a323af5aabdb47991
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-03-18 11:20:13 +0100

    Added audiosystem

---
+++ Integrian/AudioSystem.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifndef INTEGRIAN_AUDIOSYSTEM_H
+#define INTEGRIAN_AUDIOSYSTEM_H
+
+namespace Integrian
+{
+	class AudioSystem abstract
+	{
+	};
+}
+
+#endif // !INTEGRIAN_AUDIOSYSTEM_H
