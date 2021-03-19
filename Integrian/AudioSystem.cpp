10803450a38d5432f0de20681197b00c9710fed7
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-03-19 11:14:04 +0100

    Implemented base AudioSystem class

--- Integrian/AudioSystem.cpp
+++ Integrian/AudioSystem.cpp
@@ -1,1 +1,60 @@
 #include "AudioSystem.h"
+#include "Logger.h"
+
+void Integrian::AudioSystem::AddSound(const SoundID uniqueSoundID, const std::string& filePath)
+{
+#ifdef _DEBUG
+	Logger& logger{ Logger::GetInstance() };
+	if (m_Sounds.find(uniqueSoundID) != m_Sounds.cend())
+	{
+		// this soundID has already been used
+		logger.Log("SoundID: ", ErrorLevel::warning); // TODO: IMPROVE THIS RHIDIAN
+		logger.Log(std::to_string(uniqueSoundID), ErrorLevel::warning);
+		logger.Log(" was already used and has not been added\n", ErrorLevel::warning);
+	}
+	else
+	{
+		Mix_Chunk* pTemp{ Mix_LoadWAV(filePath.c_str()) };
+		if (pTemp == nullptr)
+		{
+			logger.Log(filePath, ErrorLevel::error);
+			logger.Log(" did not contain a sound file\n", ErrorLevel::error);
+		}
+		else
+		{
+			m_Sounds.insert(std::make_pair(uniqueSoundID, pTemp));
+		}
+	}
+#else
+	m_Sounds.insert(std::make_pair(uniqueSoundID, Mix_LoadWAV(filePath.c_str())));
+#endif // _DEBUG
+}
+
+void Integrian::AudioSystem::AddMusic(const SoundID uniqueMusicID, const std::string& filePath)
+{
+#ifdef _DEBUG
+	Logger& logger{ Logger::GetInstance() };
+	if (m_Music.find(uniqueMusicID) != m_Music.cend())
+	{
+		// this soundID has already been used
+		logger.Log("MusicID: ", ErrorLevel::warning); // TODO: IMPROVE THIS RHIDIAN
+		logger.Log(std::to_string(uniqueMusicID), ErrorLevel::warning);
+		logger.Log(" was already used and has not been added\n", ErrorLevel::warning);
+	}
+	else
+	{
+		Mix_Music* pTemp{ Mix_LoadMUS(filePath.c_str()) };
+		if (pTemp == nullptr)
+		{
+			logger.Log(filePath, ErrorLevel::error);
+			logger.Log(" did not contain a sound file\n", ErrorLevel::error);
+		}
+		else
+		{
+			m_Music.insert(std::make_pair(uniqueMusicID, pTemp));
+		}
+	}
+#else
+	m_Music.insert(std::make_pair(uniqueMusicID, Mix_LoadMUS(filePath.c_str())));
+#endif // _DEBUG
+}
--- Integrian/AudioSystem.h
+++ Integrian/AudioSystem.h
@@ -3,10 +3,28 @@
 #ifndef INTEGRIAN_AUDIOSYSTEM_H
 #define INTEGRIAN_AUDIOSYSTEM_H
 
+#include <cstdint> // uint64_t
+#include <SDL_mixer.h> // Mix_Chunk, Mix_Music, more SDL_Mixer related functionality
+#include <string> // std::string
+#include <unordered_map> // std::unordered_map
+
 namespace Integrian
 {
 	class AudioSystem abstract
 	{
+	public:
+		using SoundID = uint64_t;
+
+		virtual ~AudioSystem() = default;
+
+		void AddSound(const SoundID uniqueSoundID, const std::string& filePath);
+		void AddMusic(const SoundID uniqueMusicID, const std::string& filePath);
+
+		virtual void PlaySound(const SoundID, const float = 100.f) = 0;
+
+	private:
+		std::unordered_map<SoundID, Mix_Chunk*> m_Sounds;
+		std::unordered_map<SoundID, Mix_Music*> m_Music;
 	};
 }
 
