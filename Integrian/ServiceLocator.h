a7671d0e11e1e68d9201e4942b812ad31506f007
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-03-21 18:07:46 +0100

    added some template magic, and a potential use for sfinae

--- Integrian/ServiceLocator.h
+++ Integrian/ServiceLocator.h
@@ -31,27 +31,33 @@ namespace Integrian
 			return static_cast<NullService*>(m_pNullService);
 		}
 
-		template<typename Type> // this is templated so it can be checked at compile time
+		// the commented part is sfinae
+		template<typename Type/*, typename = std::enable_if_t<std::is_base_of_v<IService, Type>>*/> // this is templated so it can be checked at compile time
 		inline static void Provide(Type* pService)
 		{
-			static_assert(std::is_base_of_v<IService, Type>, "The pointer supplied to ServiceLocator::Provide was not a Service"); // compile time assertion
-			Logger& logger{ Logger::GetInstance() };
-			if (pService == nullptr || std::is_same_v<Type, NullService>) // literally this if-statement is the reason I made this templated
+			if constexpr (std::is_base_of_v<IService, Type>)
 			{
-				logger.Log("A NullService is always present in the ServiceLocator!", ErrorLevel::warning);
-				return; // we already inserted a nullservice
-			}
+				Logger& logger{ Logger::GetInstance() };
+				if (pService == nullptr || std::is_same_v<Type, NullService>) // literally this if-statement is the reason I made this templated
+				{
+					logger.Log("A NullService is always present in the ServiceLocator!", ErrorLevel::warning);
+					return; // we already inserted a nullservice
+				}
 
-			if (m_pServices.find(pService) != m_pServices.cend())
-			{
-				logger.Log("The service: ", ErrorLevel::warning);
-				logger.Log(typeid(pService).name(), ErrorLevel::warning);
-				logger.Log(" was already provided", ErrorLevel::warning);
+				if (m_pServices.find(pService) != m_pServices.cend())
+				{
+					logger.Log("The service: ", ErrorLevel::warning);
+					logger.Log(typeid(pService).name(), ErrorLevel::warning);
+					logger.Log(" was already provided", ErrorLevel::warning);
+				}
+				else
+					m_pServices.insert(pService);
 			}
 			else
-				m_pServices.insert(pService);
+			{
+				static_assert(std::is_base_of_v<IService, Type>, "The pointer supplied to ServiceLocator::Provide was not a Service"); // compile time assertion
+			}
 		}
-
 
 	private:
 		inline static std::unordered_set<IService*> m_pServices{};
