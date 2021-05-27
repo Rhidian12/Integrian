3c6e8c851e6ed1f2f143a534f9bca395462c8b48
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-05-27 21:18:33 +0200

    subject commented out

--- Integrian/Subject.h
+++ Integrian/Subject.h
@@ -8,45 +8,45 @@
 
 namespace Integrian
 {
-	class IObserver;
-	class Subject
-	{
-	public:
-		Subject() = default;
-		~Subject() = default;
-
-		template<typename ... Args>
-		void AddObserver(Observer<Args...>* pObserver)
-		{
-			ObserverConstIt it{ std::find(m_pObservers.cbegin(),m_pObservers.cend(),pObserver) };
-			if (it == m_pObservers.cend())
-				m_pObservers.push_back(pObserver);
-			else
-				Logger::LogError("Observer was already added!\n");
-		}
-
-		template<typename ... Args>
-		void RemoveObserver(Observer<Args...>* pObserver)
-		{
-			m_pObservers.erase(std::remove(m_pObservers.begin(), m_pObservers.end(), pObserver), m_pObservers.end());
-		}
-
-		template<typename ... Args>
-		inline void Notify(const std::string& event, const Args& ... args)
-		{
-			for (size_t i{}; i < m_pObservers.size(); ++i)
-			{
-				Observer<Args...>* pTemplatedObserver{ dynamic_cast<Observer<Args...>*>(m_pObservers[i]) };
-				if(pTemplatedObserver)
-					pTemplatedObserver->OnNotify(event, args...);
-			}
-		}
-
-	private:
-		std::vector<IObserver*> m_pObservers;
-
-		using ObserverConstIt = std::vector<IObserver*>::const_iterator;
-	};
+	//class IObserver;
+	//class Subject
+	//{
+	//public:
+	//	Subject() = default;
+	//	~Subject() = default;
+	//
+	//	template<typename ... Args>
+	//	void AddObserver(Observer<Args...>* pObserver)
+	//	{
+	//		ObserverConstIt it{ std::find(m_pObservers.cbegin(),m_pObservers.cend(),pObserver) };
+	//		if (it == m_pObservers.cend())
+	//			m_pObservers.push_back(pObserver);
+	//		else
+	//			Logger::LogError("Observer was already added!\n");
+	//	}
+	//
+	//	template<typename ... Args>
+	//	void RemoveObserver(Observer<Args...>* pObserver)
+	//	{
+	//		m_pObservers.erase(std::remove(m_pObservers.begin(), m_pObservers.end(), pObserver), m_pObservers.end());
+	//	}
+	//
+	//	template<typename ... Args>
+	//	inline void Notify(const std::string& event, const Args& ... args)
+	//	{
+	//		for (size_t i{}; i < m_pObservers.size(); ++i)
+	//		{
+	//			Observer<Args...>* pTemplatedObserver{ dynamic_cast<Observer<Args...>*>(m_pObservers[i]) };
+	//			if(pTemplatedObserver)
+	//				pTemplatedObserver->OnNotify(event, args...);
+	//		}
+	//	}
+	//
+	//private:
+	//	std::vector<IObserver*> m_pObservers;
+	//
+	//	using ObserverConstIt = std::vector<IObserver*>::const_iterator;
+	//};
 }
 
 #endif // !INTEGRIAN_SUBJECT_H
